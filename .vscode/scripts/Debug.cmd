86a5916fdfb54034bda4abd7d82f8a5b97849c3c
illoOminated <illoominated@gmail.com>
2020-09-28 18:45:39 +0200

    Make auto-startable by run command

--- .vscode/scripts/Debug.cmd
+++ .vscode/scripts/Debug.cmd
@@ -1,8 +1,10 @@
 cd ..\..\FarmList
 set SourcePath=%CD%
-set TargetPath="F:\World of Warcraft\_classic_\Interface\AddOns\"
-mkdir %TargetPath%\FarmList
-xcopy /S /E /Y %SourcePath%\* %TargetPath%\FarmList\*
-%TargetPath%\..\..\WoWClassic.exe
+set TargetPath="F:\World of Warcraft\_classic_\Interface\AddOns\FarmList"
+mkdir %TargetPath%
+xcopy /Y %SourcePath%\FarmList.lua %TargetPath%\FarmList\FarmList.lua
+xcopy /Y %SourcePath%\FarmList.toc %TargetPath%\FarmList\FarmList.toc
+xcopy /Y %SourcePath%\FarmList.xml %TargetPath%\FarmList\FarmList.xml
+%TargetPath%\..\..\..\WoWClassic.exe
 
 
